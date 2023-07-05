d1863281798e2a85771976ae824da4e0ab3f9903
JohanExsitec <johan.arlinger@exsitec.se>
2023-07-05 13:47:20 +0200

    Implemented title movement by mouse

--- script.js
+++ script.js
@@ -1,17 +1,41 @@
-/*----------------MOUSE MOVEMENT------------------*/
-document.addEventListener('mousemove', function(event) {
-    var cursorX = event.clientX;
-    var cursorY = event.clientY;
-    console.log('Cursor position:', cursorX, cursorY);
-  });
+/*----------------MOUSE MOVEMENT ------------------*/
+
+//document.addEventListener('mousemove', function(event) {
+function mouseMovement(event) {
+  var x = event.clientX;
+  var y = event.clientY;
+  var cursor = {x, y};
+  return cursor;
+};
   
 /*-----------CALCULATE MIDDLE OF SCREEN-----------*/
-var screenWidth = window.innerWidth;
-var screenHeight = window.innerHeight;
-var midpointX = screenWidth / 2;
-var midpointY = screenHeight / 2;
-console.log('Midpoint of the screen:', midpointX, midpointY);
+function middleOfScreen() {
+  var screenWidth = window.innerWidth;
+  var screenHeight = window.innerHeight;
+  var x = screenWidth / 2;
+  var y = screenHeight / 2;
+  var midPoint = {x, y};
+return midPoint;
+}
+
+function setTextPosition(event) {
+  text  = document.getElementById('name-holder');
 
 
+  let midpoint = middleOfScreen();
+  let cursor = mouseMovement(event);
+  let diffX = ((midpoint.x - (text.offsetWidth/2))- (cursor.x/(100)));
+  let diffY = ((midpoint.y - (text.offsetHeight/2))- (cursor.y/(100)));
+  text.style.left = diffX + "px";
+  text.style.top = diffY + "px";
+}
+
+
+
+
+
+document.addEventListener('mousemove', function(event) {
+setTextPosition(event);
+});
 
   
