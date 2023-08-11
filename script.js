5ef2d043bb19358b25b8033f37ce6a495860e3c4
JohanExsitec <johan.arlinger@exsitec.se>
2023-08-11 12:34:53 +0200

    Removed the canvas idea and scrapped it in this project. Started making balls

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
+let balls = [];
+
+
 document.addEventListener('DOMContentLoaded', () => {
 /*----------------MOUSE MOVEMENT ------------------*/
 
-//document.addEventListener('mousemove', function(event) {
-function mouseMovement(event) {
-  var x = event.clientX;
-  var y = event.clientY;
-  var cursor = {x, y};
-  return cursor;
-};
-  
+
+let mouse = {x:0, y:0};
+
+document.addEventListener('mousemove', (event) => {
+  mouse = mouseMovement(event);
+})
 /*-----------CALCULATE MIDDLE OF SCREEN-----------*/
 function middleOfScreen() {
   var screenWidth = window.innerWidth;
@@ -19,63 +20,72 @@ function middleOfScreen() {
 return midPoint;
 }
 
-
-
-
-
-  
-//canvas coding
-//run after it is loaded
-
-  // Get the canvas element
-  const canvas = document.getElementById('drawing-board');
-  // Get the 2D rendering context
-  const ctx = canvas.getContext('2d');
-  //Set background color
-
-  //Function declarations
-  function setBackgroundColor(){
-    ctx.fillStyle = '#090914';
-    ctx.fillRect(0 , 0, canvas.width, canvas.height);
-  }
-
-  function setTextPosition(event) {
-    
-  
-  
-    let midpoint = middleOfScreen();
-    let cursor = mouseMovement(event);
-    let diffX = ((midpoint.x )+ (cursor.x/(100)));
-    let diffY = ((midpoint.y )+ (cursor.y/(100)));
-    // Set the font style
-    ctx.font = "bold 24px 'IBM Plex Mono', monospace";
-    ctx.fillText("björnfoot", diffX, diffY);
-  }
-
-
-
-  //Initialize everything to be drawn
-  
-  function draw() {
-    // Clear the canvas
-    setBackgroundColor();
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-   
-    //Update text to be drawn
-
-    //Adding eventlistener so that it gets its functionality
-    document.addEventListener('mousemove', function(event) {
-      setTextPosition(event);
-      });
-
-
-
-    // Request the next frame
-    requestAnimationFrame(draw);
-
-  }
-
-  // Start the animation loop
-  draw();
+// Remove all balls if the right click is hit
+document.addEventListener("contextmenu", function(event) {
+  // Prevent the default context menu from showing up
+  event.preventDefault();
+  let allBalls = Array.from(document.getElementsByClassName('ball'));
+  allBalls.forEach(ball => {ball.remove()});
+  ballCounter = 0;
+  balls.length = 0;
 });
+
+//Eventlistener to the document of adding divs.
+let ballCounter = 0;
+let background = document.getElementById('background')
+
+
+function addBall() {
+
+  // Godot checkout
+  
+  const ballRadius = 25;
+  const ballWeight = 3;
+  let ballPosition = {x: mouse.x - ballRadius, y: mouse.y - ballRadius };
+
+  let ball = document.createElement('div');
+  ball.setAttribute('id', `ball-${ballCounter}`);
+  ball.classList.add('ball');
+
+  ball.style.width = 2*ballRadius + 'px';
+  ball.style.height = 2*ballRadius + 'px'; 
+  
+
+  ball.style.top =  ballPosition.y + 'px';
+  ball.style.left = ballPosition.x + 'px';
+
+  background.appendChild(ball);
+  ballCounter++;
+
+}
+
+document.addEventListener('click', () => {
+  addBall();
+  })
+})
+
+function mouseMovement(event) {
+  var x = event.clientX;
+  var y = event.clientY;
+  var cursor = {x, y};
+  return cursor;
+};
+  
+
+
+
+//Main tick function
+const intervalMilliseconds = 16; // approximately 60 frames per second
+
+function tick() {
+  // Your code to be executed at every tick goes here
+
+  // For example, you could update the game state, animation, etc.
+}
+
+// Start the interval
+const intervalID = setInterval(tick, intervalMilliseconds);
+
+// To stop the interval (for example, when you want to pause the tick):
+// clearInterval(intervalID);
 
